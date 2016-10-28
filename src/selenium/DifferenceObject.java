eaaaa284a246a69ddcc02cfaa2658854dc7eede0
natefikru <natefikru@gmail.com>
2016-10-28 11:20:31 -0500

    fixed spacing problem

--- src/selenium/DifferenceObject.java
+++ src/selenium/DifferenceObject.java
@@ -1,5 +1,35 @@
 package selenium;
 
-public class DifferenceObject {
+import java.util.*;
 
+public class DifferenceObject {
+	
+	
+	HashMap<String, String> baseItems = new HashMap<String, String>();
+	HashMap<String, String> cycleItems = new HashMap();
+	
+	ArrayList baseList = new ArrayList();
+	ArrayList cycleList = new ArrayList();
+
+	public DifferenceObject(){
+		
+	}
+	
+	public HashMap<String,String> getBaseHashItem(int i){
+		HashMap<String, String> localMap = (HashMap<String, String>) this.baseList.get(i);
+		return localMap;
+	}
+	
+	public void putBaseHashItem(HashMap<String, String> hash){
+		this.baseList.add(hash);
+	}
+	
+	public HashMap<String,String> getCycleHashItem(int i){
+		HashMap<String, String> localMap = (HashMap<String, String>) this.cycleList.get(i);
+		return localMap;
+	}
+	
+	public void putCycleHashItem(HashMap<String, String> hash){
+		this.cycleList.add(hash);
+	}
 }
--- src/selenium/SeleniumExtractor.java
+++ src/selenium/SeleniumExtractor.java
@@ -14,6 +14,9 @@ public class SeleniumExtractor {
 	List<String> baseDifferences = new ArrayList<String>();
 	WebElement thdbody;
 	List<WebElement> baseLineTrNumber;
+	List <WebElement> cycleLineTrNumber;
+	
+	DifferenceObject differenceObject = new DifferenceObject();
 	
 	public SeleniumExtractor(){
 		initializeDriver();
@@ -24,6 +27,7 @@ public class SeleniumExtractor {
 		thd.get("C:\\Users\\a69753\\Documents\\secondSample.html");
 		this.thdbody =	thd.findElement(By.xpath("//tbody[@class= 'u2d_tbody']"));
 		this.baseLineTrNumber= thdbody.findElements(By.xpath("//tr[@class='u2d_x_del']"));
+		this.cycleLineTrNumber= thdbody.findElements(By.xpath("//tr[@class='u2d_x_add']"));
 	}
 	
 	
@@ -31,68 +35,68 @@ public class SeleniumExtractor {
 		
 		  //---------------------------------------------CycleFileDifferences-----------------------------//
 				
-			System.out.println("Differences from Cycle file");
-				
-			List <WebElement> cycleLineTrNumber= thdbody.findElements(By.xpath("//tr[@class='u2d_x_add']"));
-			//System.out.println(cycleLineTrNumber.size());
+		System.out.println("Differences from Cycle file");
+		//System.out.println(cycleLineTrNumber.size());
 			
-			for (WebElement eachCycleTrNumber:cycleLineTrNumber )
+		for (WebElement eachCycleTrNumber:cycleLineTrNumber )
+		{
+			List<WebElement> tdTagsinCycleTr=eachCycleTrNumber.findElements(By.tagName("td"));
+			
+			for(WebElement tdTaginCycleTr: tdTagsinCycleTr)
 			{
-				List<WebElement> tdTagsinCycleTr=eachCycleTrNumber.findElements(By.tagName("td"));
-				
-				for(WebElement tdTaginCycleTr: tdTagsinCycleTr)
+				List<WebElement> cycleSpans=tdTaginCycleTr.findElements(By.tagName("span"));
+				List<String>cycleSpanText= new ArrayList<String>();
+				for (WebElement cycleSpan: cycleSpans)
 				{
-					List<WebElement> cycleSpans=tdTaginCycleTr.findElements(By.tagName("span"));
-					List<String>cycleSpanText= new ArrayList<String>();
-					for (WebElement cycleSpan: cycleSpans)
-					{
-						String cycleSpanDifference= cycleSpan.getText();
-						cycleSpanText.add(cycleSpanDifference);
-						//System.out.println(cycleSpanDifference);
-					}
-					StringBuilder cycleString= new StringBuilder();
-					for (String cs:cycleSpanText)
-					{
-						cycleString.append(cs);
-						System.out.print(cs);
-					}
-					String cString = cycleString.toString();
-					this.cycleDifferences.add(cString);
+					String cycleSpanDifference= cycleSpan.getText();
+					cycleSpanText.add(cycleSpanDifference);
+					//System.out.println(cycleSpanDifference);
 				}
-				System.out.println("");
+				StringBuilder cycleString= new StringBuilder();
+				for (String cs:cycleSpanText)
+				{
+					cycleString.append(cs);
+//					System.out.print(cs);
+				}
+				String cString = cycleString.toString();
+				this.cycleDifferences.add(cString);
 			}
-
+			System.out.println("");
 		}
+
+	}
 	
 	public void baseLineFileDifferences(){
 		//----------------------------------BaseLineFileDifferences----------------------------------//
+		
+		System.out.println("Differences from Base file");
     	 
 		for (WebElement eachBaseTrNumber:baseLineTrNumber)
 	    {
-			List<WebElement> tdTagsinBaseTr= eachBaseTrNumber.findElements(By.tagName("td"));
-			for (WebElement tdTaginBaseTr: tdTagsinBaseTr)
+			String lineNumber = eachBaseTrNumber.findElement(By.xpath("//td[@class='u2d_ln u2d_x_del_ln']")).getText();
+			System.out.println(lineNumber);
+			System.out.println("red");
+			System.out.println(eachBaseTrNumber.getText());
+			WebElement tdTagsinBaseTr= eachBaseTrNumber.findElement(By.xpath("//td[@class='u2d_code']"));
+
+			List<WebElement> spanElements = tdTagsinBaseTr.findElements(By.tagName("span"));
+			List<String> spanText= new ArrayList<String>();
+			
+			StringBuilder spanString = new StringBuilder();
+			
+			for (WebElement span:spanElements)
 			{
-				
-				List<WebElement> spanElements=tdTaginBaseTr.findElements(By.tagName("span"));
-				List<String> spanText= new ArrayList<String>();
-				
-				for (WebElement span:spanElements)
-				{
-					String difference= span.getText();
-					spanText.add(difference);
-				}
-				
-				//int var=spanText.size();
-				StringBuilder strng= new StringBuilder();
-				for (String s: spanText)
-				{
-					strng.append(s);
-					System.out.print(s);
-				}
-				String bString = strng.toString();
-				this.baseDifferences.add(bString);
+				String difference= span.getText();
+				spanString.append(difference);
 			}
-			System.out.println("");
+			
+			String bString = spanString.toString();
+			
+			System.out.println(bString);
+			this.baseDifferences.add(bString);
+			System.out.println("*************");
+			
+			
 	    }
 			
 	}
